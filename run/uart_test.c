7e064cb1a0802d75a9cff39abdea4284181d796c
Tommy Thorn <tommy.thorn@esperantotech.com>
2021-12-09 11:06:59 -0800

    run/uart_test: don't depend on headers
    To make this slightly easier to build, don't depend of standard
    headers as they aren't present by default for some cross tool chains.

--- run/uart_test.c
+++ run/uart_test.c
@@ -1,30 +1,30 @@
 // Code based on Sifive HIFive uart base https://wiki.osdev.org/HiFive-1_Bare_Bones
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdio.h>
-
 /* UART */
 #define UART0_CTRL_ADDR 0x54000000UL
 #define UART_REG_TXFIFO         0x00
 
+typedef unsigned char u8;
+typedef unsigned int  u32;
+typedef unsigned long size_t;
+
 /* This function will read a 32-bit value from an MMIO register */
-static inline uint32_t mmio_read_u32(unsigned long reg, unsigned int offset) {
-  return (*(volatile uint32_t *) ((reg) + (offset)));
+static inline u32 mmio_read_u32(unsigned long reg, unsigned int offset) {
+  return (*(volatile u32 *) ((reg) + (offset)));
 }
 
 /* This function will write a byte to an MMIO register */
-static inline void mmio_write_u8(unsigned long reg, unsigned int offset, uint8_t val) {
-  (*(volatile uint32_t *) ((reg) + (offset))) = val;
+static inline void mmio_write_u8(unsigned long reg, unsigned int offset, u8 val) {
+  (*(volatile u32 *) ((reg) + (offset))) = val;
 }
 
 /*This function will write a 32-bit value to an MMIO register */
-static inline void mmio_write_u32(unsigned long reg, unsigned int offset, uint32_t val) {
-  (*(volatile uint32_t *) ((reg) + (offset))) = val;
+static inline void mmio_write_u32(unsigned long reg, unsigned int offset, u32 val) {
+  (*(volatile u32 *) ((reg) + (offset))) = val;
 }
 
 /* Transmit a single byte over the UART */
-static void __uart_write(uint8_t byte) {
+static void __uart_write(u8 byte) {
   /* wait for the UART to become ready */
   while (mmio_read_u32(UART0_CTRL_ADDR, UART_REG_TXFIFO) & 0x80000000)
     ;
@@ -34,7 +34,7 @@ static void __uart_write(uint8_t byte) {
 }
 
 /* Transmit a buffer of length "len" over the UART */
-static void uart_write(uint8_t *buf, size_t len) {
+static void uart_write(u8 *buf, size_t len) {
   int i;
   for (i = 0; i < len; i ++) {
     __uart_write(buf[i]);
@@ -57,7 +57,7 @@ static size_t strlen(char *str) {
 }
 
 /* Write a null-terminated string to the UART, transmitting it */
-static void uart_write_string(uint8_t *buf) {
+static void uart_write_string(u8 *buf) {
   uart_write(buf, strlen((char *) buf));
 }
 
